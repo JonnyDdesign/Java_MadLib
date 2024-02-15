bb45f896dff554b6eb9be7271b9eeedecc8b807c
JonnyDdesign <jondesserres@gmail.com>
2024-02-15 10:12:45 -0700

    Rewrote the story to make it original.

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,27 +1,27 @@
-public class MadLibs {
+public class MadLib {
     /*
   This program generates a mad lib story.
   Author: Jon Desserres
   Date: 2/14/2024
     */
     public static void main(String[] args){
-        java.lang.String direction1 = "east";
-        String adjective1 = "tall";
-        String adjective2 = "fast";
-        String adjective3 = "delicious";
-        String verb1 = "hop";
-        String noun1 = "park";
-        String noun2 = "hammer";
-        String noun3 = "taco";
-        String noun4 = "table";
-        String noun5 = "cat";
-        String noun6 = "cactus";
-        String name2 = "Bert";
-        int number = 8;
-        String place1 = "Barbados";
+        java.lang.String name1 = "Johnny";
+        java.lang.String name2 = "SuperDunne";
+        java.lang.String adjective1 = "excited";
+        java.lang.String adjective2 = "wacky";
+        java.lang.String adjective3 = "humongous";
+        java.lang.String adjective4 = "strange";
+        java.lang.String verb1 = "shout";
+        java.lang.String noun1 = "big mouth bass";
+        java.lang.String noun2 = "indian fry bread";
+        java.lang.String noun3 = "taco";
+        java.lang.String noun4 = "drone";
+        java.lang.String noun5 = "shrimp";
+        java.lang.String time1 = "noon";
+        java.lang.String place1 = "Japan";
 
         //The template for the story
-        String story = "As the sun began to rise from the "+direction1+", woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+        String story = "As the sun began to rise on a chilly spring morning, "+name1+" woke up feeling "+adjective1+". He sensed the day was going to be a "+adjective2+" day! Outside, a bunch of people gathered around Desert Breeze Lake staring at the "+noun1+" that was jumping out of the lake. The group stood in awe as a food truck pulled up and began their daily sales of "+noun2+". The cook began to "+verb1+" orders to the crowd to see who was interested in the delicious grub. The people lined up for a taste of the food and were particularly interested in the "+noun3+"s, which was the special of the day. As the people got their food, they made their way back to the lake and noticed a "+noun4+" was circling overhead. "+name1+" pointed at the "+adjective3+" object in the sky and was in awe as the crowd cheered. Meanwhile, "+name2+" was texting his friends nearby about the spectacle and heard a "+adjective4+" sound off in the distance. Before long, the sound was getting closer and closer. Just as the clock struck "+time1+", the sound caught everyone's attention and the lake began swirling. "+name1+" and "+name2+" looked at each other and knew exactly what was happening. These signs meant the giant "+noun5+" were waking up from their decade-long slumber. Before long, people were running away from the lake confused as to what was happening. "+name1+" and "+name2+" knew they would be hungry as they awoke and knew exactly what they needed to feed them. They raced over to the food truck and asked for 100 orders of "+noun3+"s. The cooks began to feverishly prepare the order as the boys looked at the lake to make sure all was well. The order was ready and the boys headed to the lake to prepare the feast. They knew the feast is just what they needed before they head out on their journey around the world to "+place1+".";
         System.out.println(story);
     }
 }
